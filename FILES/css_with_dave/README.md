f76780f9ad5bf5aa474c16d7f7f84841c818c152
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-04-19 12:23:14 +0300

    organising files

---
+++ FILES/css_with_dave/README.md
@@ -0,0 +1,11 @@
+## Introduction to css
+
+What is css?
+- CSS stands for Cascading Style Sheet. 
+- It is a style sheet language used for describing the look and feel of a document written in HTML.
+- The goal of CSS is to separate content from visual elements, allowing web developers to create websites with a consistent appearance without having to embed styles directly
+- The browser uses CSS to control how HTML elements are rendered on screen.
+
+##### How does it work?
+- A web page consists of two main parts: content (HTML) and presentation (CSS).
+- 
---
+++ week_6_javascript/index.js
@@ -0,0 +1,124 @@
+
+// Javascript
+// console.log("Hello, World!");
+
+// variables
+// how to define 
+
+// ~ using var keyword
+// var name = "Hello, World!";
+// console.log("var keyword: ", name);
+
+// ~ using let keyword
+// let MAX = 600;
+// console.log("let keyword: ", MAX);
+
+// ~ using const keyword
+// used to define variables that do not change
+// const PI = 3.14;
+// console.log("cost keyword: ", PI);
+
+// * Naming convention in Javascript
+//    - variable names can contain letters (a-z), digits (0-9) and underscores (_).
+//    - they cannot start with a digit.
+//    - they are case sensitive (i.e., MyName is different from myname).
+
+//~ camelCase
+// first letter lowercase, then capitalized words
+//* example: myVariableName
+
+// ~ snake_case
+// all lowercase letters with words separated by an underscore
+//* example: my_variable_name
+
+// ~ kebab-case
+// all lowercase letters with words separated by a hyphen
+//* example: my-variable-name
+
+// ! Datatypes in Javascritpts
+// console.log("\n Js data types");
+
+// ~ String data type 
+// Eg:- var name = "Kioko"; // String Kioko
+// var name = "Kioko";
+// console.log("string data type: ", name)
+
+//  ~ Number data types
+// Eg.- var age = 25; // number 25
+
+// ~ Boolean Data Type
+// It has two values true or false
+// Eg.- var isOldEnoughToVote = true; // boolean true
+// var success = false;
+// var success = true;
+// console.log("boolean data type: " + success);
+
+// ~ integer data type
+//  var age = 12; 
+// var age = 12;
+// console.log("integer data type: " + age);
+
+// ~ float data type
+//  var salary = 12.56;
+// var salary = 12.45;
+// console.log("Float data type: " + salary);
+
+
+// ~ Null Value
+// A special value that denotes 'no value' or 'no object'.
+// var carMake = null; //null
+// console.log("Null: ", carMake);
+
+// ~ Undefined Value
+// Used when a variable is declared but not defined yet.
+// var model; //undefined
+// console.log("Undefined: ", model); 
+
+// console.log(name);
+
+
+
+// * Javascript arthimetic operators
+// + addition
+// - substraction
+// * multiplication
+//  / division (returns decimal)
+// ** exponentiation
+// ++ increament
+// -- decrement
+// % modulus (reminder)
+
+// ~example program
+// console.log("Js Operators");
+
+// let x = 4;
+// let y = 1;
+
+// console.log("addition of x + y: ", x + y);
+// console.log("subtraction of x - y: ", x - y);
+// console.log("multiplication of x * y: ", x * y);
+// console.log("division of x / y: ", x / y );
+// console.log("modulus of x % y: ", x % y);
+
+// y++; //post increment
+// // console.log("Post Incremented y :", y);
+
+// x--; //pre increment
+// // console.log("Pre Incremented x :", x);
+
+// exponentiation
+// console.log("Exponentiation : ", Math.pow(x,y));
+
+// ~ Assignemt operator
+// equal = 
+// k = 10;
+
+// addition +=
+// k += 1;
+// console.log("Add and assign : ", k);
+
+// -= substract and assign
+// k -=2 ;
+// console.log("Sub and assign : ", k);
+
+// m=5;
